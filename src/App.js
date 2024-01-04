3d34b2ce82294fa3ad20cf1f9620a0dae37d0135
jybaek96 <qorwjddus96@naver.com>
2024-01-04 22:54:13 +0900

    refactor: style 관련 공통 props 사용 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { SplitScreen } from "./components/split-screen";
 
-const LeftSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "red" }}>{title}</h2>;
+const SideComponent = ({ title, backgroundColor }) => {
+  return <h2 style={{ backgroundColor }}>{title}</h2>;
 };
 
-const RightSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "green" }}>{title}</h2>;
-};
-
-// 위 방법(props)보다 자식 요소를 하위 구성요소로 전달하는 방법이 훨씬 더 좋음
 function App() {
   return (
     <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComponent title={"i am left"} />
-      <RightSideComponent title={"i am right"} />
+      <SideComponent title={"i am left"} backgroundColor={"green"} />
+      <SideComponent title={"i am right"} backgroundColor={"red"} />
     </SplitScreen>
   );
 }
