46cc220954b75a75220df2d673ff08b5ba2c7827
jybaek96 <qorwjddus96@naver.com>
2024-01-05 22:39:46 +0900

    refactor: 섹션2 레이아웃 관련 컴포넌트 폴더 분리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { LargeBookListItem } from "./components/2-8.list/books/LargeListItems";
-import { Modal } from "./components/2-10.modal/Modal";
-import { books } from "./components/2-8.list/data/books";
-
 function App() {
-  return (
-    <>
-      <Modal>
-        <LargeBookListItem book={books[0]} />
-      </Modal>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
