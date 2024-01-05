bf33cba38f3bd59b39376ae9652025a149954614
jybaek96 <qorwjddus96@naver.com>
2024-01-05 22:33:24 +0900

    2-10) 모달 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import { LargeAuthorListItems } from "./components/2-8.list/authors/LargeListItems";
 import { LargeBookListItem } from "./components/2-8.list/books/LargeListItems";
-import { NumberedList } from "./components/2-8.list/lists/Numbered";
-import { RegularList } from "./components/2-8.list/lists/Regular";
-import { SmalBookListItem } from "./components/2-8.list/books/SmallListItems";
-import { SmallAuthorListItems } from "./components/2-8.list/authors/SmallListItems";
-import { authors } from "./components/2-8.list/data/authors";
+import { Modal } from "./components/2-10.modal/Modal";
 import { books } from "./components/2-8.list/data/books";
 
 function App() {
   return (
     <>
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={SmallAuthorListItems}
-      />
-      <NumberedList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={LargeAuthorListItems}
-      />
-      <RegularList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={SmalBookListItem}
-      />
-      <RegularList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={LargeBookListItem}
-      />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
     </>
   );
 }
