100a872ed4e1ae4f74c3dae2223b531628166578
jybaek96 <qorwjddus96@naver.com>
2024-01-06 10:19:09 +0900

    3-14. id별 사용자 정보 API 호출 컨테이너 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { CurrentUserLoader } from "./3-container/3-13.currentUser/current-user-loader";
 import { UserInfo } from "./3-container/components/user-info";
+import { UserLoader } from "./3-container/3-14.user/user-loader";
 
 function App() {
   return (
     <>
-      <CurrentUserLoader>
+      <UserLoader userId={"1"}>
         <UserInfo />
-      </CurrentUserLoader>
+      </UserLoader>
     </>
   );
 }
