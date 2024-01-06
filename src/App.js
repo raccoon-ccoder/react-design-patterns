fcc9ebdb81d7889abcaa6165c6cdaf1a40a02537
jybaek96 <qorwjddus96@naver.com>
2024-01-06 10:25:34 +0900

    3-14. datasource 정보 API 호출 컨테이너 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { BookInfo } from "./3-container/components/book-info";
+import { ResourceLoader } from "./3-container/3-15.datasource/resource-loader";
 import { UserInfo } from "./3-container/components/user-info";
-import { UserLoader } from "./3-container/3-14.user/user-loader";
 
 function App() {
   return (
     <>
-      <UserLoader userId={"1"}>
+      <ResourceLoader resourceUrl={"/users/2"} resourceName={"user"}>
         <UserInfo />
-      </UserLoader>
+      </ResourceLoader>
+      <ResourceLoader resourceUrl={"/books/1"} resourceName={"book"}>
+        <BookInfo />
+      </ResourceLoader>
     </>
   );
 }
